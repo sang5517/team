20b81d0a6eba19ec0c40ae972269c268fdcdfd28
deka2904 <114542800+deka2904@users.noreply.github.com>
2023-11-13 17:50:36 +0900

    test

---
+++ src/main/java/com/TeamProject/TeamProject/MainController.java
@@ -0,0 +1,14 @@
+package com.TeamProject.TeamProject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(){
+        return "test";
+    }
+}
