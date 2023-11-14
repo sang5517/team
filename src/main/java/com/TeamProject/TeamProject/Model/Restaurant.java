1d940167f72449f1c08d438b808de302aaa0063e
deka2904 <114542800+deka2904@users.noreply.github.com>
2023-11-14 12:24:11 +0900

    1114test

--- src/main/java/com/TeamProject/TeamProject/Model/Restaurant.java
+++ src/main/java/com/TeamProject/TeamProject/Model/Restaurant.java
@@ -1,25 +1,25 @@
-//package com.TeamProject.TeamProject.Model;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//
-//@Entity
-//@Getter
-//@Setter
-//public class Restaurant {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id; //pk
-//
-//
-//    private Long rating; //별점
-//    private String RestaurantName; //식당이름
-//    private String cuisine; //카테고리. 예를들면 프랑스음식이면 French로 저장됨.
-//    private String address; // 식당주소
-//
-//}
+package com.TeamProject.TeamProject.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //pk
+
+
+    private Long rating; //별점
+    private String RestaurantName; //식당이름
+    private String cuisine; //카테고리. 예를들면 프랑스음식이면 French로 저장됨.
+    private String address; // 식당주소
+
+}
